4d65d5f6b2c9e932a2e03ef929e8f669b36ff215
HelloThereBeauty <talad65215@naprb.com>
2026-01-31 15:05:38 +0530

    Level Looping.

--- Folders/Scripts/level_manager.gd
+++ Folders/Scripts/level_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var loop_levels: bool = false
+
 var levels := [
 	{"path": "res://Folders/Scenes/Levels/level_1.tscn", "transition_time": 1.0},
 	{"path": "res://Folders/Scenes/Levels/level_2.tscn", "transition_time": 1.5}
@@ -13,20 +15,22 @@ func load_level(index: int) -> void:
 	var t = level_data.transition_time
 
 	get_tree().paused = true
-	
 	await Transaction.fade_in(t)
 
 	get_tree().change_scene_to_file(level_data.path)
-	
 	await get_tree().process_frame
-	
-	await Transaction.fade_out(t)
 
+	await Transaction.fade_out(t)
 	get_tree().paused = false
 
 func next_level() -> void:
 	current_level += 1
-	if current_level < levels.size():
-		load_level(current_level)
-	else:
-		print("All levels finished")
+
+	if current_level >= levels.size():
+		if loop_levels:
+			current_level = 0
+		else:
+			print("All levels finished")
+			return
+
+	load_level(current_level)
